e2a775838be9bc8ee4e8b51a047bdb36988b7c17
chao-hua <chaohua@outlook.com>
2018-10-23 17:22:20 +0800

    update

--- examples/main.js
+++ examples/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './app.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './app.vue';
+import router from './router';
 import JasmineView from '@/index';
-import '@/styles/index.scss'
+import '@/styles/index.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(JasmineView);
 
 new Vue({
     router,
     render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
