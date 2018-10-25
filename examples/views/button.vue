1def64ac807881e19eefb9dfa0446746cd842e6c
chao-hua <chaohua@outlook.com>
2018-10-25 16:26:23 +0800

    update

--- examples/views/button.vue
+++ examples/views/button.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="button">
-        <js-button @click="test">测试</js-button>
+        <js-button @click="test">测试1</js-button>
+        <js-button >测试2</js-button>
     </div>
 </template>
 <script>
