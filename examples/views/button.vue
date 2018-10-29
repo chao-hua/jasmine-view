1aa144e03a975cb5620a728baef7351214f87403
chao-hua <chaohua@outlook.com>
2018-10-29 17:20:46 +0800

    update

--- examples/views/button.vue
+++ examples/views/button.vue
@@ -9,6 +9,22 @@
             <js-button disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
+            <js-button size="medium">默认按钮</js-button>
+            <js-button size="medium" plain>朴素按钮</js-button>
+            <js-button size="medium" round>圆角按钮</js-button>
+            <js-button size="medium" circle>圆</js-button>
+            <js-button size="medium" disabled>禁用</js-button>
+            <js-button size="medium" disabled plain>朴素禁用</js-button>
+        </div>
+        <div class="row">
+            <js-button size="small">默认按钮</js-button>
+            <js-button size="small" plain>朴素按钮</js-button>
+            <js-button size="small" round>圆角按钮</js-button>
+            <js-button size="small" circle>圆</js-button>
+            <js-button size="small" disabled>禁用</js-button>
+            <js-button size="small" disabled plain>朴素禁用</js-button>
+        </div>
+        <div class="row">
             <js-button type="text">默认按钮</js-button>
             <js-button type="text" plain>朴素按钮</js-button>
             <js-button type="text" round>圆角按钮</js-button>
