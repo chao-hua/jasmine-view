ebead562ef0079a876b9f628312cd78cec9b25fa
chao-hua <chaohua@outlook.com>
2018-10-29 17:23:08 +0800

    update

--- examples/views/button.vue
+++ examples/views/button.vue
@@ -9,7 +9,7 @@
             <js-button disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button size="medium">默认按钮</js-button>
+            <js-button size="medium">中型按钮</js-button>
             <js-button size="medium" plain>朴素按钮</js-button>
             <js-button size="medium" round>圆角按钮</js-button>
             <js-button size="medium" circle>圆</js-button>
@@ -17,7 +17,7 @@
             <js-button size="medium" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button size="small">默认按钮</js-button>
+            <js-button size="small">小型按钮</js-button>
             <js-button size="small" plain>朴素按钮</js-button>
             <js-button size="small" round>圆角按钮</js-button>
             <js-button size="small" circle>圆</js-button>
@@ -25,7 +25,7 @@
             <js-button size="small" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button type="text">默认按钮</js-button>
+            <js-button type="text">文字按钮</js-button>
             <js-button type="text" plain>朴素按钮</js-button>
             <js-button type="text" round>圆角按钮</js-button>
             <js-button type="text" circle>圆</js-button>
@@ -33,7 +33,7 @@
             <js-button type="text" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button type="primary">默认按钮</js-button>
+            <js-button type="primary">primary</js-button>
             <js-button type="primary" plain>朴素按钮</js-button>
             <js-button type="primary" round>圆角按钮</js-button>
             <js-button type="primary" circle>圆</js-button>
@@ -41,7 +41,7 @@
             <js-button type="primary" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button type="success">默认按钮</js-button>
+            <js-button type="success">success</js-button>
             <js-button type="success" plain>朴素按钮</js-button>
             <js-button type="success" round>圆角按钮</js-button>
             <js-button type="success" circle>圆</js-button>
@@ -49,7 +49,7 @@
             <js-button type="success" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button type="warning">默认按钮</js-button>
+            <js-button type="warning">warning</js-button>
             <js-button type="warning" plain>朴素按钮</js-button>
             <js-button type="warning" round>圆角按钮</js-button>
             <js-button type="warning" circle>圆</js-button>
@@ -57,7 +57,7 @@
             <js-button type="warning" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button type="danger">默认按钮</js-button>
+            <js-button type="danger">danger</js-button>
             <js-button type="danger" plain>朴素按钮</js-button>
             <js-button type="danger" round>圆角按钮</js-button>
             <js-button type="danger" circle>圆</js-button>
@@ -65,7 +65,7 @@
             <js-button type="danger" disabled plain>朴素禁用</js-button>
         </div>
         <div class="row">
-            <js-button type="info">默认按钮</js-button>
+            <js-button type="info">info</js-button>
             <js-button type="info" plain>朴素按钮</js-button>
             <js-button type="info" round>圆角按钮</js-button>
             <js-button type="info" circle>圆</js-button>
