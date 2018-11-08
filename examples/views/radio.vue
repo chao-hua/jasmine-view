7032e27ad8c96a0127603e5ffa1b57ade7ce6671
chao-hua <chaohua@outlook.com>
2018-11-08 17:18:50 +0800

    update

--- examples/views/radio.vue
+++ examples/views/radio.vue
@@ -92,21 +92,4 @@ export default {
 .row {
     margin: 10px 0;
 }
-
-.item div {
-    background-color: #9999CC;
-    border-radius: 4px;
-    height: 40px;
-    line-height: 40px;
-    padding: 0 5px;
-    text-align: center;
-}
-
-.item-2 div {
-    background-color: rgba(#9999CC, .6);
-}
-
-.item-3 div {
-    background-color: rgba(#9999CC, .3);
-}
 </style>
