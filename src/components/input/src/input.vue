c522a25ba05db6090a9cd21008fa7eed68f7217e
chao-hua <chaohua@outlook.com>
2018-11-15 17:22:50 +0800

    update

--- src/components/input/src/input.vue
+++ src/components/input/src/input.vue
@@ -1,70 +1,42 @@
 <template>
-    <input type="text" :value="currentValue" @input="handleInput" @focus="handleFocus" @blur="handleBlur" @change="handleChange">
+    <div class="js-input">
+        <input type="text" :value="currentValue" @input="handleInput" @focus="handleFocus" @blur="handleBlur" @change="handleChange">
+    </div>
 </template>
 <script>
 import Emitter from 'mixins/emitter.js'
+import { oneOf } from 'utils/util.js'
 export default {
     name: 'JsInput',
     componentName: 'JsInput',
     mixins: [Emitter],
     props: {
-        value: {},
+        value: [String, Number],
+        disabled: Boolean,
+        clearable: Boolean,
+        size: {
+            type: String,
+            validator(val) {
+                return oneOf(val, ['medium', 'small']);
+            }
+        },
+        type: {
+            type: String,
+            default: 'text',
+            validator(val) {
+                return oneOf(val, ['text', 'textarea']);
+            }
+        },
+        max: Number,
+        min: Number,
+        placeholder: String
     },
     data() {
         return {
             currentValue: this.value === undefined || this.value === null ? '' : this.value,
         }
     },
-    computed: {
-        isGroup() {
-            let parent = this.$parent;
-            while (parent) {
-                if (parent.$options.componentName !== 'JsCheckboxGroup') {
-                    parent = parent.$parent;
-                } else {
-                    this._checkboxGroup = parent;
-                    return true;
-                }
-            }
-            return false;
-        },
-        model: {
-            get() {
-                return this.isGroup ? this._checkboxGroup.value : this.value;
-            },
-            set(val) {
-                if (this.isGroup) {
-                    this.isLimited = false;
-                    if (this._checkboxGroup.min !== undefined && val.length < this._checkboxGroup.min)
-                        this.isLimited = true;
-                    if (this._checkboxGroup.max !== undefined && val.length > this._checkboxGroup.max)
-                        this.isLimited = true;
-                    !this.isLimited && this.dispatch('JsCheckboxGroup', 'input', val);
-                } else {
-                    this.$emit('input', val);
-                }
-            }
-        },
-        isChecked() {
-            if (Object.prototype.toString.call(this.model) === '[object Boolean]') {
-                return this.model;
-            } else if (Array.isArray(this.model)) {
-                return this.model.indexOf(this.label) > -1;
-            }
-        },
-        isDisabled() {
-            return this.isGroup ? this._checkboxGroup.disabled || this.disabled : this.disabled;
-        },
-        tabIndex() {
-            return this.isDisabled ? -1 : (this.isGroup ? (this.model === this.label ? 0 : -1) : 0);
-        },
-        checkboxSize() {
-            return this.isGroup ? this._checkboxGroup.size || '' : '';
-        },
-        isButton() {
-            return this.isGroup ? this._checkboxGroup.button || '' : '';
-        }
-    },
+    computed: {},
     methods: {
         handleChange(ev) {
             this.$emit('change', ev.target.value);
